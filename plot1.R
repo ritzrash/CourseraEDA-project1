e89217a7fedeb9dafc4c6cb052d07933e381fb35
Ritika <ritika030820@gmail.com>
2020-07-17 20:32:27 +0530

    Plot1 Final

--- plot1.R
+++ plot1.R
@@ -2,5 +2,5 @@ df <- read.table("D:/EDA coursera/household_power_consumption.txt", header = TRU
 df_subset <- df[df$Date %in% c("1/2/2007","2/2/2007"),]
 GlobalActivePower <- as.numeric(df_subset$Global_active_power)
 png(file = "plot1.png", width = 480, height = 480)
-hist(GlobalActivePower, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts")
+hist(GlobalActivePower, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
